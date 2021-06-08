80af7ebc07c950fc583e6ac2515706ad8a9dd699
Antonio José Masiá <email@antoniomasia.com>
2021-06-08 20:42:40 +0200

    Initial commit

---
+++ src/main/router.ts
@@ -0,0 +1,5 @@
+const Router = require('@cyyjs/electron-router');
+
+Router.get('hello', () => {
+  return 'Electron + Vue 3 + TypeScript + Vite!'
+})
---
+++ src/render/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img class="logo" alt="Vue logo" src="./assets/vite.svg" />
+  <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+  <img class="logo" alt="Vue logo" src="./assets/electron.png" />
+  <HelloWorld :msg="msg" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import { getHello } from './api';
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    getHello().then((res: any) => {
+      this.msg = res.data
+    })
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  width: 150px;
+  margin-right: 20px;
+}
+</style>
